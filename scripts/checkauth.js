15bcbb6397a501d9faaaed2ce5a29dcf78a652d0
Álvaro García <alvaro.garcia.f@gmail.com>
2020-07-01 00:44:02 +0100

    checkauth.js - added login from when user not logged

--- scripts/checkauth.js
+++ scripts/checkauth.js
@@ -1,6 +1,41 @@
 
 // USER NOT LOGGED
 if (!localStorage.getItem('token')) {
+  document.getElementById('login-modal').innerHTML = `
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+
+        <div class="modal-header border-primary">
+          <h5 class="modal-title">Welcome back!</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span class="text-primary" aria-hidden="true">&times;</span>
+          </button>
+        </div>
+
+        <div class="modal-body">
+          <form id="loginForm">
+            <div class="form-group">
+              <label for="loginEmail">Email</label>
+              <input type="email" class="form-control" id="loginEmail" placeholder="example@example.com" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+              <label for="loginPassword">Password</label>
+              <input type="password" class="form-control" id="loginPassword" autocomplete="true">
+            </div>
+            <div class="text-center">
+              <button type="submit" class="btn btn-primary" id="loginBtn">Submit</button>
+            </div>
+          </form>
+        </div>
+
+        <div class="modal-footer justify-content-center">
+          <span>You don't have an account? <a href="#" class="text-primary" data-dismiss="modal" data-toggle="modal" data-target="#signUpModal">Register here</a></span>
+        </div>
+
+      </div>
+    </div>
+  `
+
   document.getElementById('myNavbar').innerHTML = `    
     <a href="index.html" class="navbar-brand d-flex align-items-center"><strong>Iddle</strong> </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader">
@@ -28,7 +63,7 @@ if (!localStorage.getItem('token')) {
         </li>
         <div class="dropdown-divider border-primary"></div>
         <li class="navbar-item">
-          <button class="btn btn-primary" type="button" data-toggle="modal" data-target="#logInModal">Get Started</button>
+          <button class="btn btn-primary" type="button" data-toggle="modal" data-target="#login-modal">Get Started</button>
         </li>
       </ul>
     </div>
