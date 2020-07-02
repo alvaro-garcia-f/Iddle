451c34445fe4f2ab9e2dcef35f16d84f10444ea1
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-07-02 22:40:12 +0100

    search.html styling fixed, and index.html also fixed to match search card

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,32 @@
-function viewVideo (id) {  
+function viewVideo (id) {
   localStorage.setItem('videoId', id)
-  window.location.href = 'video.html' 
+  window.location.href = 'video.html'
 }
 $(document).ready(function () {
 
   API.get('/videos/mostwatched')
-    .then(response => {    
+    .then(response => {
       let code = ''
       let count = 0
-      response.data.forEach(element => {                
-        code += ` 
-              <div class="carousel-item ${count === 0 ? 'active' : ''}">
-               <div class="card box-shadow">
-               <a href='javascript: viewVideo(${JSON.stringify(element._id)})' ><img class="card-img-top" src="http://i3.ytimg.com/vi/${element.url.split('/')[4]}/maxresdefault.jpg"></a>
-                 <div class="card-body">
-                   <div class="row">
-                     <p class="col-12 card-text font-weight-bold">${element.title}</p>                     
-                   </div>
-                   <div class="d-flex justify-content-between pt-1">
-                      <div>
-                        <small>Author Name</small> <br>
-                        <span class="bg-white border-muted small"><i class="fa fa-eye text-primary" aria-hidden="true"></i><small> ${element.views}</small></span>
-                      </div>
-                      <div>
-                        <small class="col card-text justify-content-end small">${element.level}</small>
-                        <br>
-                        <small class="text-muted">${element.duration} mins</small>
-                      </div>
-                    </div>
-                 </div>
-               </div>
-             </div>
-               `
+      response.data.forEach(element => {
+        code += `
+          <div class="carousel-item ${count === 0 ? 'active' : ''}">
+            <div class="card box-shadow">
+            <a href='javascript: viewVideo(${JSON.stringify(element._id)})' ><img class="card-img-top" src="http://i3.ytimg.com/vi/${element.url.split('/')[4]}/maxresdefault.jpg"></a>
+            <div class="card-body">
+              <p class="card-text font-weight-bold">${element.title}</p>
+              <div class="d-flex justify-content-between pt-1">
+                <small>Author Name</small>
+                <small class="card-text">${element.level}</small>
+              </div>
+              <div class="d-flex justify-content-between pt-1">
+                <span class="bg-white border-muted small"><i class="fa fa-eye text-primary" aria-hidden="true"></i><small> ${element.views}</small></span>
+                <small class="text-muted">${element.duration} mins</small>
+              </div>
+            </div>
+            </div>
+          </div>
+            `
         count++
       })
       document.getElementById('carousel-most-watched').innerHTML = code
@@ -43,25 +38,20 @@ $(document).ready(function () {
       let code = ''
       let count = 0
       response.data.forEach(element => {
-        code += ` 
+        code += `
           <div class="carousel-item ${count === 0 ? 'active' : ''}">
             <div class="card box-shadow">
               <img class="card-img-top" src="http://i3.ytimg.com/vi/${element.url.split('/')[4]}/maxresdefault.jpg">
               <div class="card-body">
-                <div class="row">
-                  <p class="col-12 card-text font-weight-bold">${element.title}</p>                     
-                </div>
-                <div class="d-flex justify-content-between pt-1">
-                  <div>
-                    <small>Author Name</small> <br>
-                    <span class="bg-white border-muted small"><i class="fa fa-eye text-primary" aria-hidden="true"></i><small> ${element.views}</small></span>
-                  </div>
-                  <div>
-                    <small class="col card-text justify-content-end small">${element.level}</small>
-                    <br>
-                    <small class="text-muted">${element.duration} mins</small>
-                  </div>
-                </div>
+                <p class="card-text font-weight-bold">${element.title}</p>
+              <div class="d-flex justify-content-between pt-1">
+                <small>Author Name</small>
+                <small class="card-text">${element.level}</small>
+              </div>
+              <div class="d-flex justify-content-between pt-1">
+                <span class="bg-white border-muted small"><i class="fa fa-eye text-primary" aria-hidden="true"></i><small> ${element.views}</small></span>
+                <small class="text-muted">${element.duration} mins</small>
+              </div>
               </div>
             </div>
           </div>
--- scripts/search-results.js
+++ scripts/search-results.js
@@ -12,19 +12,18 @@ API
     let code = ''
     response.data.forEach(element => {
       code += `
-        <div class="col-md-4 p-3">
+        <div class="col-md-4 pb-2">
           <div class="card box-shadow">
             <img class="card-img-top" src="http://i3.ytimg.com/vi/${element.url.split('/')[4]}/maxresdefault.jpg">
             <div class="card-body">
-              <div class="row">
-                <p class="col-8 card-text">${element.title}</p>
-                <p class="col-4 card-text justify-content-end small">${element.level}</p>
+                <p class="card-text font-weight-bold">${element.title}</p>
+              <div class="d-flex justify-content-between pt-1">
+                <small>Author Name</small>
+                <small class="card-text">${element.level}</small>
               </div>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div> <small class="text-muted">${element.duration} mins</small>
+              <div class="d-flex justify-content-between pt-1">
+                <span class="bg-white border-muted small"><i class="fa fa-eye text-primary" aria-hidden="true"></i><small> ${element.views}</small></span>
+                <small class="text-muted">${element.duration} mins</small>
               </div>
             </div>
           </div>
